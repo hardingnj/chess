6e1c7f6db8ece8bc430f80b4b643300eeaa2d76c
Nicholas Harding <nharding@oll-nharding.(none)>
2014-02-23 12:17:24 -0500

    rearranged order of commands so can do stuff while downloading

--- DEPLOY.sh
+++ DEPLOY.sh
@@ -2,12 +2,14 @@
 # This bash script simply kicks off the 3 containers required to run the evaluation.
 
 # first pull the necessary images/ most recent versions.
-sudo docker pull hardingnj/sqlcontainer
-sudo docker pull hardingnj/parsepgn
-sudo docker pull hardingnj/scorepgn
 
+sudo docker pull hardingnj/sqlcontainer
 sudo docker run -d -name sqlserver -p 3305:3305 -v mysql:/var/lib/mysql hardingnj/sqlcontainer
-sleep 40
+sleep 20;
+
+sudo docker pull hardingnj/parsepgn
 sudo docker run -d -t -link sqlserver:db -name parsePGN -v /glusterfs/users/nharding/pgn_data:/pgn:ro hardingnj/parsepgn
-sleep 15
+sleep 20;
+
+sudo docker pull hardingnj/scorepgn
 sudo docker run -d -t -link sqlserver:db -name scorePGN hardingnj/scorepgn
