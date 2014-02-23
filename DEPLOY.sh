2a64196f810f9d3f4cf9e6d65aa75afc24b6a9aa
Nicholas Harding <nharding@oll-nharding.(none)>
2014-02-23 11:52:56 -0500

    updated DEPLOY for OSDC cluster
    also deleted RUN and TODO.

--- DEPLOY.sh
+++ DEPLOY.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
-sudo bash sql_container/run-server.sh
-sleep 5
-sudo bash parsePGN/run-container.sh
-sleep 5
-sudo bash scorePGN/run-client.sh
+# This bash script simply kicks off the 3 containers required to run the evaluation.
+
+# first pull the necessary images/ most recent versions.
+sudo docker pull hardingnj/sqlcontainer
+sudo docker pull hardingnj/parsepgn
+sudo docker pull hardingnj/scorepgn
+
+sudo docker run -d -name sqlserver -p 3305:3305 -v mysql:/var/lib/mysql hardingnj/sqlcontainer
+sleep 40
+sudo docker run -d -t -link sqlserver:db -name parsePGN -v /glusterfs/users/nharding/pgn_data:/pgn:ro hardingnj/parsepgn
+sleep 15
+sudo docker run -d -t -link sqlserver:db -name scorePGN hardingnj/scorepgn
