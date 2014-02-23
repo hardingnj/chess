6a0623e84d4d0d51fe1ef4e155f22741460798f9
Nicholas Harding <nharding@oll-nharding.(none)>
2014-02-23 10:44:32 -0500

    added code to work out SAN of best move and write to db

--- scorePGN/scorePGN.pl
+++ scorePGN/scorePGN.pl
@@ -8,7 +8,7 @@ use Term::ANSIColor;
 use YAML qw/LoadFile Dump/;
 use Chess::Rep;
 use DBI;
-my $VERSION = "0.03";
+my $VERSION = "0.04";
 
 my $cfg = LoadFile("/opt/settings.yaml");
 
@@ -60,33 +60,35 @@ do {
       );
     
     my @algebraic_moves = split(/,/, $game->{algebraic_moves});
-    
     my @coordinate_moves;
     my @move_scores;
     my @move_mate_in;
+
+    my @opt_algebraic_moves;
     my @opt_coordinate_moves;
     my @opt_move_scores;
     my @opt_move_mate_in;
     
     my $pos = Chess::Rep->new;
-    my $current_state;
+    my $current_fen;
     my $halfmovecounter = 0;
     # loop through coordinate_moves: 
     #   do analysis
     foreach my $move (@algebraic_moves) { 
     	# set current state as FEN
-    	$current_state = $pos->get_fen;
+    	$current_fen = $pos->get_fen;
     	++$halfmovecounter;
     	
     	# update board with current move & capture move info
     	my $status = $pos->go_move($move);
-    	my $move_uci_format = $status->{from}.$status->{to}; 
+    	my $move_as_coordinates = $status->{from}.$status->{to}; 
     
-        push @coordinate_moves, $move_uci_format;
+        push @coordinate_moves, $move_as_coordinates;
 
     	# code to skip 3 lines if too early in game
     	if ($cfg->{EvalAfter} >= ($halfmovecounter/2)) {
               push @opt_coordinate_moves, 'NA';
+              push @opt_algebraic_moves,  'NA';
               push @opt_move_scores,      'NA';
               push @move_scores,          'NA';
               push @move_mate_in,         'NA';
@@ -94,11 +96,16 @@ do {
               next;
               }
              
-    	my $playedmove = choosemove(fen => $current_state, searchmoves => [$move_uci_format], depth => $cfg->{Depth});
+    	my $playedmove = choosemove(fen => $current_fen, searchmoves => [$move_as_coordinates], depth => $cfg->{Depth});
     	my $bestmove   = choosemove(depth => $cfg->{Depth});
     
+		# dummy pos
+		my $dummy_position = Chess::Rep->new($current_fen);
+        my $hypothet_move  = $dummy_position->go_move($bestmove->{move});
+
     	# on occasion played nove scores higher than best move...
         push @opt_coordinate_moves, $bestmove->{move}; 
+        push @opt_algebraic_moves,  $hypothet_move->{san};
         push @opt_move_scores,      $bestmove->{cp};
         push @move_scores,          $playedmove->{cp};
         push @move_mate_in,         $playedmove->{matein}; 
@@ -110,9 +117,9 @@ do {
     die "something went wrong..." if !@move_scores;
 
     $dbh->do(
-      'UPDATE games SET processed = ?, coordinate_moves = ?, move_scores = ?, opt_coordinate_moves = ?, opt_move_scores = ?, move_mate_in = ?, opt_move_mate_in = ?, time_s = ? WHERE id = ?',
+      'UPDATE games SET processed = ?, coordinate_moves = ?, move_scores = ?, opt_algebraic_moves = ?, opt_coordinate_moves = ?, opt_move_scores = ?, move_mate_in = ?, opt_move_mate_in = ?, time_s = ? WHERE id = ?',
       undef,
-      1, join(',', @coordinate_moves), join(',', @move_scores), join(',', @opt_coordinate_moves), join(',', @opt_move_scores), join(',', @move_mate_in), join(',', @opt_move_mate_in), $end - $start, $game->{id}
+      1, join(',', @coordinate_moves), join(',', @move_scores), join(',', @opt_algebraic_moves), join(',', @opt_coordinate_moves), join(',', @opt_move_scores), join(',', @move_mate_in), join(',', @opt_move_mate_in), $end - $start, $game->{id}
       );
     
     # end of loop.
@@ -165,6 +172,7 @@ sub choosemove {
 		depth => 20,
 		movetime => undef,
 		searchmoves => [],
+		verbose => 0,
 		@_
 		);
 	my %data; #holds the output
@@ -175,19 +183,19 @@ sub choosemove {
 	$command .= " depth " . $args{depth} if defined $args{depth};
 	my @searchmoves = @{$args{searchmoves}};
 	if(@searchmoves) { $command .= ' searchmoves '; $command .= join(' ', @searchmoves); } 
-	print "Debug: $command".$/;
+	print "Debug: $command".$/ if $args{verbose};
 
-	print $Engine "position fen $args{fen}\n" if defined $args{fen};
-	print $Engine $command.$/;
+	print $Engine "position fen $args{fen}\n" if (defined $args{fen} and $args{verbose});
+	print $Engine $command.$/ if $args{verbose};
 	while (<$Reader>) {
 
 		my $line = $_;
 		$line =~ s/\n|\r//g;
 
 		if ($line =~ m/multipv/) {
-			print color 'red';
-			print "$line\n";
-			print color 'reset';
+			print color 'red' if $args{verbose};
+			print "$line\n" if $args{verbose};
+			print color 'reset' if $args{verbose};
 			$multipv = $line;
 			}
  		if ($line =~ m/bestmove /) {
@@ -229,15 +237,4 @@ sub choosemove {
 			}
 	}
 	die "did not read...";
-}
-
-# --------------------------------------------------------------------------------------------------
-sub get_host_ip {
-# --------------------------------------------------------------------------------------------------
-# Parse the ENV hash to get ip
-	while((my $key, my $value) = each(%ENV)) {
-		print $key . '> ' . $value . $/;
-		if($value =~ m/(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/){ return ($1); }
-		}
-	die "Host ip not found in ENV";
 }
