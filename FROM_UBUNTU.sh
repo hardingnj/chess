9e918738738bc42a525270cc686880aa3801b360
Nicholas Harding <nharding@oll-nharding.(none)>
2014-03-10 22:50:48 -0400

    adding install docker and run score script

--- FROM_UBUNTU.sh
+++ FROM_UBUNTU.sh
@@ -1,7 +1,7 @@
 # install docker
 sudo sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 sudo apt-get update
-sudo apt-get install lxc-docker
+sudo apt-get install -y --force-yes lxc-docker
 
 sudo docker pull hardingnj/scorepgn
 HOSTLOC=/glusterfs/users/nharding/
