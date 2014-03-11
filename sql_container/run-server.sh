47a068ecbaf6865c08760dee307d4b2feeb10f61
Nicholas Harding <nharding@oll-nharding.(none)>
2014-03-10 22:54:28 -0400

    Deleting sql container, replaced by sqlite.

--- sql_container/run-server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-HOSTSQLDATA="/data/mysql"
-# -d = daemon, -name for later linking -p for port -v for volume, we map host /data/mysql to /var/lib/mysql
-docker run -d -name sqlserver -p 3305:3305 -v $HOSTSQLDATA:/var/lib/mysql mysql
