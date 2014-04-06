8f0ec6619073d692bbae8086031374676a2d59e9
Nicholas Harding <nharding@oll-nharding.(none)>
2014-04-06 14:30:25 -0400

    Fixed typos in run on OSDC

--- parsePGN/run-parsepgn-osdc.sh
+++ parsePGN/run-parsepgn-osdc.sh
@@ -2,5 +2,5 @@
 # This is a simple bash script to run the parse container.
 # - rm command kills as soon as complete.
 # PATH IS THE LOCATION ON THE HOST OF THE PGN FILES
-HOSTPATH=/glusterfs/users/nharding/pgn_data
+HOSTPATH=/glusterfs/users/nharding
 docker run -d -t -v ${HOSTPATH}/pgn_data:/pgn:ro -v ${HOSTPATH}/chessDB:/data hardingnj/parsepgn $@
